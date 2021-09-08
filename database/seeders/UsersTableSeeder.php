e586bb06ded66dca134524123095d6c537117edb
Liahera <311.lsr@gmail.com>
2021-09-09 00:22:33 +0300

    add seeds

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        /*
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);*/
+        User::factory()
+            ->count(50)
+            ->create();
+        Task::factory()
+            ->count(50)
+            ->create();
+    }
+
+}
